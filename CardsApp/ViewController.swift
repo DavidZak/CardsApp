337ade516ff794907c3f6ba46d4b48ce63c58d5d
davidzakharyan <davka126@mail.ru>
2020-01-28 13:30:46 +0400

    init

---
+++ CardsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CardsApp
+//
+//  Created by David Zakharyan on 28.01.2020.
+//  Copyright © 2020 David Zakharyan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
